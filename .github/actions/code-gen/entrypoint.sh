e3182b457339ac84d6f34730c6018167ae9b7f9f
endotakuya <Endooooooo7@gmail.com>
2019-12-22 05:31:52 +0900

    fix

--- .github/actions/code-gen/entrypoint.sh
+++ .github/actions/code-gen/entrypoint.sh
@@ -1,38 +1,9 @@
 #!/bin/sh
-set -e
+set -eu
 
-export JAR_PATH=./openapi-generator/code-gen/openapi-generator-cli.jar
-
-# Swagger validation
-java -jar ${JAR_PATH} validate -i swagger.yaml
-
-# remove old generated code
-rm -rf kotlin-client kotlin-server swift4_api_client typescript go
-
-# generate kotlin-server
-java -jar ${JAR_PATH} generate -i swagger.yaml -g kotlin-spring --additional-properties useBeanValidation=true --enable-post-process-file -o kotlin-server
-rm -rf ./kotlin-server/docs \
-  ./kotlin-server/README.md \
-  ./kotlin-server/build.gradle.kts \
-  ./kotlin-server/pom.xml \
-  ./kotlin-server/settings.gradle \
-  ./kotlin-server/src/main/resources \
-  ./kotlin-server/src/main/kotlin/org/openapitools/api \
-  ./kotlin-server/src/main/kotlin/org/openapitools/Application.kt \
-  ./kotlin-server/src/test
-# generate kotlin-client
-java -jar ${JAR_PATH} generate -i swagger.yaml -g kotlin -o kotlin-client
-rm -rf ./kotlin-client/docs ./kotlin-client/README.md ./kotlin-client/src/main/kotlin/org/openapitools/client/infrastructure
-# generate swift4_api_client
-java -jar ${JAR_PATH} generate -i swagger.yaml -g swift4 -o swift4_api_client
-# generate go
-java -jar ${JAR_PATH} generate -i swagger.yaml -g go-gin-server -o go
-# generate typescript
-java -jar ${JAR_PATH} generate -i swagger.yaml -g typescript-axios -o typescript
-
-git config --global user.name u-nation
-git config --global user.email Endooooooo7@gmail.com
-git remote set-url origin https://u-nation:${GITHUB_TOKEN}@github.com/u-nation/sample-openapi.git
+# git setting
+git config --global user.name github-actions
+git config --global user.email noreply@github.com
 
 export GIT_BRANCH="$(git symbolic-ref HEAD --short 2>/dev/null)"
 if [ "$GIT_BRANCH" = "" ] ; then
@@ -40,8 +11,44 @@ if [ "$GIT_BRANCH" = "" ] ; then
   export GIT_BRANCH=${GIT_BRANCH#remotes/origin/};
 fi
 
+git remote set-url origin https://team-lab:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git
 git checkout $GIT_BRANCH
-git branch -a
-git add .
-git commit -m "add generated code"
-git push origin $GIT_BRANCH
+
+# OpenAPI Generate
+export JAR_PATH=./pkg-openapi-generator/code-gen/openapi-generator-cli.jar
+## validation
+java -jar ${JAR_PATH} validate -i swagger.yaml
+## remove old generated code
+rm -rf kotlin-client kotlin-server swift typescript go-gin
+## generate kotlin-server
+java -jar ${JAR_PATH} generate -i swagger.yaml -g kotlin-spring --additional-properties useBeanValidation=true --enable-post-process-file -o kotlin-server
+rm -rf kotlin-server/docs \
+  kotlin-server/build.gradle.kts \
+  kotlin-server/pom.xml \
+  kotlin-server/settings.gradle \
+  kotlin-server/src/main/resources \
+  kotlin-server/src/main/kotlin/org/openapitools/api \
+  kotlin-server/src/main/kotlin/org/openapitools/Application.kt \
+  kotlin-server/src/test
+## generate kotlin-client
+java -jar ${JAR_PATH} generate -i swagger.yaml -g kotlin -o kotlin-client
+rm -rf kotlin-client/docs kotlin-client/src/main/kotlin/org/openapitools/client/infrastructure
+## generate swift
+java -jar ${JAR_PATH} generate -i swagger.yaml -g swift4 -o swift
+## generate go
+java -jar ${JAR_PATH} generate -i swagger.yaml -g go-gin-server -o go-gin
+## generate typescript
+java -jar ${JAR_PATH} generate -i swagger.yaml -g typescript-axios -o typescript
+## remove all README.md
+find kotlin-client kotlin-server swift typescript go-gin -name "README.md" | xargs rm
+
+# push if diff exists
+set +e
+git diff --exit-code --quiet
+if [ $? -eq 1 ]; then
+  git add .
+  git commit -m "add generated code"
+  git push origin $GIT_BRANCH
+else
+  echo "nothing to commit"
+fi
